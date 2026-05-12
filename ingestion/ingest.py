e231f3ecc9b6f81a73b842e9d70f6a6092ac563f
Poonam Gupta <poonamg24@iitk.ac.in>
2026-05-12 18:10:30 +0530

    up

--- ingestion/ingest.py
+++ ingestion/ingest.py
@@ -16,7 +16,7 @@ def get_driver():
 
 def load_data():
     print("Loading CSV...")
-    
+    #df = pd.read_csv('../data/HI-Small_Trans.csv')
     df = pd.read_csv('data/HI-Small_Trans.csv')
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     df = df[df['Timestamp'].dt.date <= pd.Timestamp('2022-09-03').date()]
